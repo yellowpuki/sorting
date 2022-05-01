6f8192fbe425a4ed6493e39393cb65f94a71b3bc
yellowpuki <32398419+yellowpuki@users.noreply.github.com>
2022-05-01 21:31:25 +0300

    add insertion sort function

--- insSort.go
+++ insSort.go
@@ -28,8 +28,26 @@ func findMax(array []int) int {
 	return maxIdx
 }
 
+func insSort(array []int) {
+	var x int
+	for i := 1; i < len(array); i++ {
+		x = array[i]
+		j := i
+
+		for ; j > 0 && array[j-1] > x; j-- {
+			array[j] = array[j-1]
+		}
+
+		array[j] = x
+	}
+}
+
 func main() {
 	var a = []int{3, 1, 5, 7, 8, 2, 6}
 	fmt.Println("Min el index ", findMin(a), " value = ", a[findMin(a)])
 	fmt.Println("Max el index ", findMax(a), " value = ", a[findMax(a)])
+
+	insSort(a)
+
+	fmt.Println("Sorted array: ", a)
 }
